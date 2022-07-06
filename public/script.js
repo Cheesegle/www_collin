7f943aafbcc230b07d7e226063387a3fea607477
Cheese <36419194+Cheesegle@users.noreply.github.com>
2022-07-06 18:33:56 +0000

    tippy tappy clicky clacky

--- public/script.js
+++ public/script.js
@@ -25,20 +25,24 @@ wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 let placemarkLayer = new WorldWind.RenderableLayer("Placemark");
 wwd.addLayer(placemarkLayer);
 
-//create placemark attributes
-
-
-
-
-//create home placemark
-
-//home placemark label
+//new highlight controller
+var highlightController = new WorldWind.HighlightController(wwd);
 
 
 //placemark class
 class Placemark {
-  constructor(lon, lat, z, color, placemarkLabel, imageSource, offsetX) {
+  constructor(lon, lat, z, color, placemarkLabel, imageSource, offsetX, modal) {
     let placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+    //placemark position
+    let position = new WorldWind.Position(lon, lat, z);
+
+    this.placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+
+    //highlight attributes
+    let highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+    this.placemark.highlightAttributes = highlightAttributes;
+
     //image offset
     placemarkAttributes.imageOffset = new WorldWind.Offset(
       WorldWind.OFFSET_FRACTION, offsetX,
@@ -55,18 +59,20 @@ class Placemark {
     //placemark image
     placemarkAttributes.imageSource = imageSource;
 
-    //placemark position
-    let position = new WorldWind.Position(lon, lat, z);
-
-    this.placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+    //placemark label
     this.placemark.label = `${placemarkLabel}\n` +
       "Lat " + this.placemark.position.latitude.toPrecision(4).toString() + "\n" +
       "Lon " + this.placemark.position.longitude.toPrecision(5).toString();
     this.placemark.alwaysOnTop = true;
 
+    //placemark modal
+    if (modal) {
+      this.placemark.userProperties.clickModal = modal;
+    }
     placemarkLayer.addRenderable(this.placemark);
   }
   toggle() {
+    //placemarks toggle
     if (this.placemark.enabled) {
       this.placemark.enabled = false;
     } else {
@@ -75,13 +81,53 @@ class Placemark {
   }
 }
 
+//placemarks array
 let placemarks = [];
 
-placemarks.push(new Placemark(36.091919, -115.294617, 100, "YELLOW", "Las Vegas Home", "images/clickbait.png", 1));
 
-placemarks.push(new Placemark(41.452040, -74.438760, 100, "GREEN", "Northern Academy", "images/plain-red.png", 0.3));
+//add placemarks
+placemarks.push(new Placemark(36.091919, -115.294617, 100, "YELLOW", "Las Vegas Home", "images/clickbait.png", 1, "LASVEGAS"));
+
+placemarks.push(new Placemark(41.452040, -74.438760, 100, "GREEN", "Northern Academy", "images/plain-red.png", 0.3, "NORTHERN"));
+
+//on placemark click open modal
+window.addEventListener('click', e => {
+  //get mouse x and y
+  var x = e.clientX,
+    y = e.clientY;
+
+  //get highlighted placemarks
+  var placeList = wwd.pick(wwd.canvasCoordinates(x, y));
+
+  //if placemarks exist loop through them and find ones with clickModal user property
+  if (placeList.objects.length > 0) {
+    for (var i = 0; i < placeList.objects.length; i++) {
+      if (placeList.objects[i].userObject && placeList.objects[i].userObject.userProperties && placeList.objects[i].userObject.userProperties.clickModal) {
+        let clickModal = placeList.objects[i].userObject.userProperties.clickModal;
+        // alert(clickModal);
+        document.getElementById(clickModal).style.display = 'block';
+      }
+    }
+  }
+})
+
+//modal close
+let closeElements = document.getElementsByClassName('close');
+
+let closeModal = function() {
+  let modalElements = document.getElementsByClassName('modal');
+  for (let i = 0; i < modalElements.length; i++) {
+    modalElements[i].style.display = 'none';
+  }
+
+};
+
+for (let i = 0; i < closeElements.length; i++) {
+  closeElements[i].addEventListener('click', closeModal, false);
+}
 
 
+//placemarks toggle button
 function placemarkToggle() {
   placemarks.forEach(placemark => {
     placemark.toggle();
