9e4bf8d1a76e7535e0a98f0f0621ba8b043c9d00
zihaowang999 <zihao.wang@g.feitianacademy.org>
2022-07-07 10:31:31 -0400

    format and change layer toggle

--- public/level_1_2/script.js
+++ public/level_1_2/script.js
@@ -31,56 +31,57 @@ var highlightController = new WorldWind.HighlightController(wwd);
 
 //placemark class
 class Placemark {
-  constructor(lon, lat, z, color, placemarkLabel, imageSource, offsetX, modal) {
-    let placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+    constructor(lon, lat, z, color, placemarkLabel, imageSource, offsetX, modal) {
+        let placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
 
-    //placemark position
-    let position = new WorldWind.Position(lon, lat, z);
+        //placemark position
+        let position = new WorldWind.Position(lon, lat, z);
 
-    this.placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+        this.placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
 
-    //image offset
-    placemarkAttributes.imageOffset = new WorldWind.Offset(
-      WorldWind.OFFSET_FRACTION, offsetX,
-      WorldWind.OFFSET_FRACTION, 0.0);
+        //image offset
+        placemarkAttributes.imageOffset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, offsetX,
+            WorldWind.OFFSET_FRACTION, 0.0);
 
-    //highlight attributes
-    let highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
-    this.placemark.highlightAttributes = highlightAttributes;
-    highlightAttributes.imageScale = 1.2;
+        //highlight attributes
+        let highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+        this.placemark.highlightAttributes = highlightAttributes;
+        highlightAttributes.imageScale = 1.2;
 
-    //label color
-    placemarkAttributes.labelAttributes.color = WorldWind.Color[color];
+        //label color
+        placemarkAttributes.labelAttributes.color = WorldWind.Color[color];
 
-    //label offset
-    placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-      WorldWind.OFFSET_FRACTION, 0.5,
-      WorldWind.OFFSET_FRACTION, 1.0);
+        //label offset
+        placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.5,
+            WorldWind.OFFSET_FRACTION, 1.0);
 
-    //placemark image
-    placemarkAttributes.imageSource = imageSource;
+        //placemark image
+        placemarkAttributes.imageSource = imageSource;
 
-    //placemark label
-    this.placemark.label = `${placemarkLabel}\n` +
-      "Lat " + this.placemark.position.latitude.toPrecision(4).toString() + "\n" +
-      "Lon " + this.placemark.position.longitude.toPrecision(5).toString();
-    this.placemark.alwaysOnTop = true;
-    this.placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+        //placemark label
+        this.placemark.label = `${placemarkLabel}\n` +
+            "Lat " + this.placemark.position.latitude.toPrecision(4).toString() + "\n" +
+            "Lon " + this.placemark.position.longitude.toPrecision(5).toString();
+        this.placemark.alwaysOnTop = true;
+        this.placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
 
-    //placemark modal user property
-    if (modal) {
-      this.placemark.userProperties.clickModal = modal;
+        //placemark modal user property
+        if (modal) {
+            this.placemark.userProperties.clickModal = modal;
+        }
+        placemarkLayer.addRenderable(this.placemark);
     }
-    placemarkLayer.addRenderable(this.placemark);
-  }
-  toggle() {
-    //placemarks toggle
-    if (this.placemark.enabled) {
-      this.placemark.enabled = false;
-    } else {
-      this.placemark.enabled = true;
+
+    toggle() {
+        //placemarks toggle
+        if (this.placemark.enabled) {
+            this.placemark.enabled = false;
+        } else {
+            this.placemark.enabled = true;
+        }
     }
-  }
 }
 
 //placemarks array
@@ -97,52 +98,58 @@ let modalOpen = false;
 
 //on placemark click open modal
 window.addEventListener('click', event => {
-  //get mouse x and y
-  var x = event.clientX,
-    y = event.clientY;
+    //get mouse x and y
+    var x = event.clientX,
+        y = event.clientY;
 
-  //get highlighted placemarks
-  var placeList = wwd.pick(wwd.canvasCoordinates(x, y));
+    //get highlighted placemarks
+    var placeList = wwd.pick(wwd.canvasCoordinates(x, y));
 
-  //find placemarks with clickModal user property
-  //also make sure to not repoen modal if modal is already open
-  if (placeList.objects.length > 0 && modalOpen === false) {
-    for (var i = 0; i < placeList.objects.length; i++) {
-      if (placeList.objects[i]?.userObject?.userProperties?.clickModal) {
-        modalOpen = true;
-        //make modal visible
-        let clickModal = placeList.objects[i].userObject.userProperties.clickModal;
-        document.getElementById(clickModal).style.display = 'block';
-      }
+    //find placemarks with clickModal user property
+    //also make sure to not repoen modal if modal is already open
+    if (placeList.objects.length > 0 && modalOpen === false) {
+        for (var i = 0; i < placeList.objects.length; i++) {
+            if (placeList.objects[i]?.userObject?.userProperties?.clickModal) {
+                modalOpen = true;
+                //make modal visible
+                let clickModal = placeList.objects[i].userObject.userProperties.clickModal;
+                document.getElementById(clickModal).style.display = 'block';
+            }
+        }
     }
-  }
 })
 
 //modal close
 let closeElements = document.getElementsByClassName('close');
 
-let closeModal = function() {
-  let modalElements = document.getElementsByClassName('modal');
-  for (let i = 0; i < modalElements.length; i++) {
-    modalElements[i].style.display = 'none';
-  }
-  //timeout to make modalOpen false after click events trigger
-  setTimeout(() => {
-    modalOpen = false;
-  });
+let closeModal = function () {
+    let modalElements = document.getElementsByClassName('modal');
+    for (let i = 0; i < modalElements.length; i++) {
+        modalElements[i].style.display = 'none';
+    }
+    //timeout to make modalOpen false after click events trigger
+    setTimeout(() => {
+        modalOpen = false;
+    });
 };
 
 for (let i = 0; i < closeElements.length; i++) {
-  closeElements[i].addEventListener('click', closeModal, false);
+    closeElements[i].addEventListener('click', closeModal, false);
 }
 for (let i = 0; i < closeElements.length; i++) {
-  closeElements[i].addEventListener('click', closeModal, false);
+    closeElements[i].addEventListener('click', closeModal, false);
 }
 
 
 //placemarks toggle button
 function placemarkToggle() {
-  placemarks.forEach(placemark => {
-    placemark.toggle();
-  })
+    // placemarks.forEach(placemark => {
+    //   placemark.toggle();
+    // })
+    //placemarks toggle
+    if (placemarkLayer.enabled) {
+        placemarkLayer.enabled = false;
+    } else {
+        placemarkLayer.enabled = true;
+    }
 }
